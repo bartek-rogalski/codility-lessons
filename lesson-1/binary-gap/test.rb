dd85580e59680bbc4c6c549079b7c6bf9a90350d
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-05 18:19:04 +0100

    Change tests to consistenly use spec syntax

--- lesson-1/binary-gap/test.rb
+++ lesson-1/binary-gap/test.rb
@@ -6,25 +6,25 @@ describe BinaryGap do
   it 'correctly returns non-zero value for valid data' do
     n = 17
     expected_longest_gap_value = 3
-    assert_equal(expected_longest_gap_value, solution(n))
+    expect(solution(n)).must_equal expected_longest_gap_value
   end
 
   it 'correctly returns zero value for valid data' do
     n = 15
     expected_longest_gap_value = 0
-    assert_equal(expected_longest_gap_value, solution(n))
+    expect(solution(n)).must_equal expected_longest_gap_value
   end
 
   it 'returns correct value for trailing zeros' do
     n = 6
     expected_longest_gap_value = 0
-    assert_equal(expected_longest_gap_value, solution(n))
+    expect(solution(n)).must_equal expected_longest_gap_value
   end
 
   it 'returns correct value when \
       the last gap in the string was not the longest one' do
     n = 1041
     expected_longest_gap_value = 5
-    assert_equal(expected_longest_gap_value, solution(n))
+    expect(solution(n)).must_equal expected_longest_gap_value
   end
 end
