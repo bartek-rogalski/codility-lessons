68001581c56648efea584133c73a19861de12114
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-06 18:05:54 +0100

    Add second parameter with river width

--- lesson-4/frog-river-one/frog_river_one.rb
+++ lesson-4/frog-river-one/frog_river_one.rb
@@ -1,6 +1,6 @@
 # https://codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
 module FrogRiverOne
-  def solution(a)
+  def solution(a, x)
     # WIP
   end
 end
--- lesson-4/frog-river-one/test.rb
+++ lesson-4/frog-river-one/test.rb
@@ -5,7 +5,8 @@ include FrogRiverOne
 describe FrogRiverOne do
   it 'correctly return result for simple input' do
     a = [1, 3, 1, 4, 2, 3, 5, 4]
+    x = 5
     seconds = 6
-    expect(solution(a)).must_equal seconds
+    expect(solution(a, x)).must_equal seconds
   end
 end
