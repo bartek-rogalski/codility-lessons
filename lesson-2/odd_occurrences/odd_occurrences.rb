3fc969057dd7c578ce58df47d7a688691bf61425
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-06 11:03:51 +0100

    Refactor for performance: sort and then compare pairs

--- lesson-2/odd_occurrences/odd_occurrences.rb
+++ lesson-2/odd_occurrences/odd_occurrences.rb
@@ -1,12 +1,9 @@
 # https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
 module OddOccurrences
   def solution(a)
-    a.each do
-      current_element = a.slice!(0)
-      id = a.find_index(current_element)
-      return current_element if id.nil?
-      a.delete_at(id)
+    a.sort!
+    (0..a.length - 1).step(2). each do |counter|
+      return a[counter] unless a[counter] == a[counter + 1]
     end
-    a[0]
   end
 end
