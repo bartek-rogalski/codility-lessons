1be3b7b9073f8abae753e532bebb6720b5c794e8
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-06 11:02:41 +0100

    Remove incorrect test case

--- lesson-2/odd_occurrences/test.rb
+++ lesson-2/odd_occurrences/test.rb
@@ -4,7 +4,7 @@ include OddOccurrences
 
 describe OddOccurrences do
   it 'returns the unique element for simple input data' do
-    a = [9, 3, 9, 3, 7, 2]
+    a = [9, 3, 9, 3, 7, 5, 5]
     expected_element = 7
     assert_equal(expected_element, solution(a))
   end
@@ -12,12 +12,6 @@ describe OddOccurrences do
   it 'correctly returns unique first element' do
     a = [6, 9, 3, 9, 3, 9, 9]
     expected_element = 6
-    assert_equal(expected_element, solution(a))
-  end
-
-  it 'returns the first unique array element only' do
-    a = [0, 1, 0, 4, 0, 0, 0]
-    expected_element = 1
     assert_equal(expected_element, solution(a))
   end
 end
