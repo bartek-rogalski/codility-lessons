b5ea5ba4be126cb6aeba141b3b9ce189e35532a9
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-06 15:42:03 +0100

    Remove debug

--- lesson-3/tape-equilibrium/tape_equilibrium.rb
+++ lesson-3/tape-equilibrium/tape_equilibrium.rb
@@ -2,13 +2,8 @@
 module TapeEquilibrium
   def solution(a)
     total_value = 0
-    puts "\n initial array"
-    print a
     a.map! { |elem| total_value += elem }
-    puts "\n after summing up"
-    print a
     a.map! { |sum| (sum - (total_value - sum)).abs }
-    puts "\n final"
     a.pop
     a.min
   end
