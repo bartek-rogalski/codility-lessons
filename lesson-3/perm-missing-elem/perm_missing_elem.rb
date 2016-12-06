d2203606fd4d679e52cc768b155d309be650c41e
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-06 12:31:37 +0100

    Check if element value matches its index in a sorted array

--- lesson-3/perm-missing-elem/perm_missing_elem.rb
+++ lesson-3/perm-missing-elem/perm_missing_elem.rb
@@ -1,6 +1,12 @@
 # https://codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
 module PermMissingElem
   def solution(a)
-    # TODO
+    counter = 1
+    a.sort!
+    a.each do |element|
+      return counter if element != counter
+      counter += 1
+    end
+    counter
   end
 end
--- lesson-3/perm-missing-elem/test.rb
+++ lesson-3/perm-missing-elem/test.rb
@@ -3,16 +3,21 @@ require 'minitest/autorun'
 include PermMissingElem
 
 describe PermMissingElem do
-
   it 'returns missing element' do
     a = [2, 3, 1, 5]
     missing_element = 4
     expect(solution(a)).must_equal missing_element
   end
 
-    it 'returns the biggest element if missing' do
+  it 'returns the biggest value if missing from array' do
     a = [2, 3, 1, 4]
     missing_element = 5
+    expect(solution(a)).must_equal missing_element
+  end
+
+  it 'returns the smallest value if missing from array' do
+    a = [2, 3, 5, 4]
+    missing_element = 1
     expect(solution(a)).must_equal missing_element
   end
 end
