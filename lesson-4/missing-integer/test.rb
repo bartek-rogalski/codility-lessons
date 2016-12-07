7f820c9dda0518fbc7f5cc38cf24e2f3e963727c
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 15:45:01 +0100

    Change test cases wording for consistency

--- lesson-4/missing-integer/test.rb
+++ lesson-4/missing-integer/test.rb
@@ -9,7 +9,7 @@ describe MissingInteger do
     expect(solution(a)).must_equal expected_missing_value
   end
 
-  it 'correctly returns result for negative-value array' do
+  it 'correctly returns result for all-negative array' do
     a = [-1, -3, -6, -4, -1, -2]
     expected_missing_value = 5
     expect(solution(a)).must_equal expected_missing_value
@@ -22,7 +22,7 @@ describe MissingInteger do
   end
 
   it 'correctly returns result one element array' do
-    a = [3]
+    a = [0]
     expected_missing_value = 1
     expect(solution(a)).must_equal expected_missing_value
   end
