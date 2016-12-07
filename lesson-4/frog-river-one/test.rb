2de18bcdbcdfbc5df142b055edbb1370ac57536d
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 12:11:35 +0100

    Fix test case to expect correct result and add more test cases

--- lesson-4/frog-river-one/test.rb
+++ lesson-4/frog-river-one/test.rb
@@ -3,10 +3,24 @@ require 'minitest/autorun'
 include FrogRiverOne
 
 describe FrogRiverOne do
-  it 'correctly return result for simple input' do
+  it 'correctly returns result for simple input' do
     a = [1, 3, 1, 4, 2, 3, 5, 4]
     x = 5
-    seconds = 6
-    expect(solution(a, x)).must_equal seconds
+    expected_time = 6
+    expect(solution(x, a)).must_equal expected_time
+  end
+
+  it 'correctly returns value if single element is needed' do
+    a = [1]
+    x = 1
+    expected_time = 0
+    expect(solution(x, a)).must_equal expected_time
+  end
+
+  it 'returns correct value when never able to cross' do
+    a = [1, 3, 2, 5]
+    x = 5
+    expected_time = -1
+    expect(solution(x, a)).must_equal expected_time
   end
 end
