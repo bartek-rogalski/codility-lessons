206e21a17ce0ea7804bb61684d23eed992ea6bc4
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 16:11:33 +0100

    Fix expected results to correct values

--- lesson-4/missing-integer/test.rb
+++ lesson-4/missing-integer/test.rb
@@ -11,13 +11,13 @@ describe MissingInteger do
 
   it 'correctly returns result for all-negative array' do
     a = [-1, -3, -6, -4, -1, -2]
-    expected_missing_value = 5
+    expected_missing_value = 1
     expect(solution(a)).must_equal expected_missing_value
   end
 
   it 'correctly returns result for mixed-value array' do
     a = [-1, -3, 6, 4, 1, 2]
-    expected_missing_value = 5
+    expected_missing_value = 3
     expect(solution(a)).must_equal expected_missing_value
   end
 
