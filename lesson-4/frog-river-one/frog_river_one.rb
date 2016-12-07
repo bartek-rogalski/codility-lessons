4a003be4d0ace789db2fb38670ef3e2e06a5a8dc
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 17:25:59 +0100

    Reduce complexity to O(n), by increasing the counter instead of counting through the array

--- lesson-4/frog-river-one/frog_river_one.rb
+++ lesson-4/frog-river-one/frog_river_one.rb
@@ -1,12 +1,16 @@
 # https://codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 module FrogRiverOne
   def solution(x, a)
-    longest = 0
-    (1..x).each do |elem|
-      current = a.find_index(elem)
-      return -1 if current.nil?
-      longest = current if current > longest
+    positions = []
+    spot = 0
+
+    a.each.with_index do |elem, seconds|
+      if positions[elem].nil?
+        positions[elem] = 0
+        spot += 1
+        return seconds if spot == x
+      end
     end
-    longest
+    -1
   end
 end
