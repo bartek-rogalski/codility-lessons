98fdd78f4b3a0ac80acbe0898ee0879a20fc7fb2
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 12:12:37 +0100

    Add correct but inefficient solution to FrogRiverOne - WIP

--- lesson-4/frog-river-one/frog_river_one.rb
+++ lesson-4/frog-river-one/frog_river_one.rb
@@ -1,6 +1,15 @@
-# https://codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+# https://codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 module FrogRiverOne
-  def solution(a, x)
-    # WIP
+  def solution(x, a)
+    occurences = []
+    time = 0
+    a.each do |pos|
+      unless occurences.include?(pos)
+        occurences << pos
+        return time if occurences.count == x
+      end
+      time += 1
+    end
+    -1
   end
 end
