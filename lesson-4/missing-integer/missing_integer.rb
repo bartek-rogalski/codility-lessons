f62d52cd7bb7fead6088b69b1de987a2c562be60
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-07 16:32:51 +0100

    Implement MissingInteger: remove non-positive or doubled values, sort the rest and find the missing value

--- lesson-4/missing-integer/missing_integer.rb
+++ lesson-4/missing-integer/missing_integer.rb
@@ -1,6 +1,12 @@
 # https://codility.com/programmers/lessons/4-counting_elements/missing_integer/
 module MissingInteger
   def solution(a)
-    # WIP
+    a.reject! { |value| value <= 0 }
+    a.uniq!
+    a.sort!
+    (1..a.length + 1).each do |elem|
+      return elem unless elem == a[(elem - 1)]
+    end
+    1
   end
 end
