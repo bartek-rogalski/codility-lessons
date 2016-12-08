93c22449619e76bd076bed9a8dcd95d97021102e
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-08 15:16:02 +0100

    Map the array instead of creating new when maxing all, performance still sub-par [WIP]

--- lesson-4/max-counters/test.rb
+++ lesson-4/max-counters/test.rb
@@ -9,6 +9,20 @@ describe MaxCounters do
     operations = [3, 4, 4, 6, 1, 4, 4]
     n = 5
     expected_counters = [3, 2, 2, 4, 2]
-    expect(solution(operations, n)).must_equal expected_counters
+    expect(solution(n, operations)).must_equal expected_counters
+  end
+
+  it 'correctly returns values if only maxing counter' do
+    operations = [10]
+    n = 3
+    expected_counters = [0, 0, 0]
+    expect(solution(n, operations)).must_equal expected_counters
+  end
+
+  it 'correctly returnes raised counters that never have been maxed' do
+    operations = [3, 4, 4, 5, 1]
+    n = 5
+    expected_counters = [1, 0, 1, 2, 1]
+    expect(solution(n, operations)).must_equal expected_counters
   end
 end
