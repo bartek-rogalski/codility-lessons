fe00a8ed3ac3d6cead95a50cf4ba9f52f6f2753d
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-16 16:50:53 +0100

    Fix typo

--- lesson-3/perm-missing-elem/perm_missing_elem.rb
+++ lesson-3/perm-missing-elem/perm_missing_elem.rb
@@ -1,11 +1,11 @@
 # https://codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
 module PermMissingElem
   def solution(array)
-    occurences = []
+    occurrences = []
     array.each do |value|
-      occurences[value - 1] = 1
+      occurrences[value - 1] = 1
     end
-    occurences << nil
-    occurences.index(nil) + 1
+    occurrences << nil
+    occurrences.index(nil) + 1
   end
 end
