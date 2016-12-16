eb2e81ce0e9803c5519fbdae43cbb3289790e094
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-16 18:16:12 +0100

    Refactor for performance to eliminate group_by method

--- lesson-2/odd_occurrences/odd_occurrences.rb
+++ lesson-2/odd_occurrences/odd_occurrences.rb
@@ -1,9 +1,16 @@
 # https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
 module OddOccurrences
   def solution(array)
-    grouped_hash = array.group_by { |element| element }
-    grouped_hash.each do |key, value|
-      return key if value.count.odd?
+    occurrences = {}
+    array.each do |element|
+      if occurrences[element].nil?
+        occurrences[element] = 1
+      else
+        occurrences[element] += 1
+      end
+    end
+    occurrences.each do |key, value|
+      return key if value.odd?
     end
   end
 end
