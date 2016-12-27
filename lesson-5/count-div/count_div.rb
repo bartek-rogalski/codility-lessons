ba58812a71858e9a92dbd2a6f7582cc525103a86
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-27 17:12:50 +0100

    Eliminate the off-one issue, add additinal test case

--- lesson-5/count-div/count_div.rb
+++ lesson-5/count-div/count_div.rb
@@ -1,9 +1,7 @@
 # https://codility.com/programmers/lessons/5-prefix_sums/count_div/
 module CountDiv
   def solution(a, b, div)
-    divisibles = (b - a) / div
-    print divisibles
-    divisibles += 1 if (a % div).zero?
-    divisibles
+    divisibles = (b / div)
+    divisibles -= (a - 1) / div
   end
 end
--- lesson-5/count-div/test.rb
+++ lesson-5/count-div/test.rb
@@ -21,4 +21,20 @@ describe CountDiv do
     expected_result = 3
     expect(solution(a, b, div)).must_equal expected_result
   end
+
+  it 'correctly returns result for maximum divisor' do
+    a = 1
+    b = 16
+    div = 16
+    expected_result = 1
+    expect(solution(a, b, div)).must_equal expected_result
+  end
+
+  it '' do
+    a = 11
+    b = 345
+    div = 17
+    expected_result = 20
+    expect(solution(a, b, div)).must_equal expected_result
+  end
 end
