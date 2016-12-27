8bfaf139161b5f67343029bfd2b7986cbe696d5f
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-27 15:35:47 +0100

    Add more test cases for edge scenarios

--- lesson-5/passing-cars/test.rb
+++ lesson-5/passing-cars/test.rb
@@ -11,4 +11,17 @@ describe PassingCars do
     expected_result = 5
     expect(solution(cars)).must_equal expected_result
   end
+
+  it 'correctly returns result for all cars going west' do
+    cars = [1, 1, 1, 1, 1]
+    expected_result = 0
+    expect(solution(cars)).must_equal expected_result
+  end
+
+  it 'correctly returns result for all cars going east' do
+    cars = [0]
+    expected_result = 0
+    expect(solution(cars)).must_equal expected_result
+  end
+
 end
