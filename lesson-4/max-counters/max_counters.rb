37779c0a1cb29c2c509502a3f38baaebfc11a0aa
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-27 14:25:27 +0100

    Eliminate map! by increasing each of the counters just when needed (on access), and at the end

--- lesson-4/max-counters/max_counters.rb
+++ lesson-4/max-counters/max_counters.rb
@@ -3,15 +3,23 @@ module MaxCounters
   def solution(n, operations)
     counters = Array.new(n, 0)
     biggest_value = 0
+    smallest_value = 0
+
     operations.each do |modification|
       element = modification - 1
       if modification == n + 1
-        counters.map! { biggest_value }
+        smallest_value = biggest_value
       else
+        counters[element] = smallest_value if counters[element] < smallest_value
         counters[element] += 1
         biggest_value = counters[element] if counters[element] > biggest_value
       end
     end
+
+    (1..counters.length - 1).each do |ind|
+      counters[ind] = smallest_value if counters[ind] < smallest_value
+    end
+    
     counters
   end
 end
