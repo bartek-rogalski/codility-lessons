18ba1454be4469d1d541c7614ad591cab2dee8da
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-27 15:31:04 +0100

    Add first passing solution for a basic test case

--- lesson-5/passing-cars/passing_cars.rb
+++ lesson-5/passing-cars/passing_cars.rb
@@ -1,6 +1,14 @@
 # https://codility.com/programmers/lessons/5-prefix_sums/passing_cars/
 module PassingCars
   def solution(cars)
-    # WIP
+    pairs = west = 0
+    sums = cars.map { |car| west += car }
+
+    cars.each_with_index do |car, ind|
+      pairs += west - sums[ind] if car.zero?
+      return -1 if pairs > 1_000_000_000
+    end
+
+    pairs
   end
 end
