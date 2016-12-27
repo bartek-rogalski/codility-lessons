39a45aeaee7b5f58344974822d7d2686cf707fe9
Bartek Rogalski <bartek.rogalski@gmail.com>
2016-12-27 12:48:22 +0100

    Correct typo

--- lesson-4/max-counters/max_counters.rb
+++ lesson-4/max-counters/max_counters.rb
@@ -3,9 +3,9 @@ module MaxCounters
   def solution(n, operations)
     counters = Array.new(n, 0)
     biggest_value = 0
-    operations.each do |modication|
-      element = modication - 1
-      if modication > n
+    operations.each do |modification|
+      element = modification - 1
+      if modification = n + 1
         counters.map! { biggest_value }
       else
         counters[element] += 1
