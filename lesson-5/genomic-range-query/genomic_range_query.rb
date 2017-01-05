97684b8123efc14067d760cfbf955c296720d189
Bartek Rogalski <bartek.rogalski@gmail.com>
2017-01-05 17:54:34 +0100

    Clean up the solution and eliminate the one-off issues

--- lesson-5/genomic-range-query/genomic_range_query.rb
+++ lesson-5/genomic-range-query/genomic_range_query.rb
@@ -1,43 +1,26 @@
 # https://codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
 module GenomicRangeQuery
-  def solution(sequence, p, q)
-    impact = { 'A' => 1, 'C' => 2, 'G' => 3, 'T' => 4}
+  def solution(sequence, beginnings, ends)
+    impact = { 'A' => 1, 'C' => 2, 'G' => 3, 'T' => 4 }
     counters = []
+    counters[0] = { 'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0 }
     results = []
 
-    sequence.each_char.with_index do |nucl, indx|
-      if indx != 0
-        counters[indx] = counters[indx - 1].dup
-      else
-        counters[indx] = {'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0}
-      end
-      counters[indx][nucl] += 1
+    sequence.each_char.with_index do |nucl, idx|
+      counters[idx + 1] = counters[idx].dup
+      counters[idx + 1][nucl] += 1
     end
 
-    p.each_with_index do |first, indx| 
-      last = q[indx]
-      # print "\n\nNEW QUERY\n\n"
-      # puts last
-      # puts first
-      # print "\n-------------"
-      if last != first
-        impact.each do |nucl, imp|
-          # print "\n\n\nNUCL " + nucl + "\n"
-          # puts 'last'
-          # puts counters[last][nucl]
-          # puts 'first'
-          # puts counters[first][nucl]
-          if ((counters[last][nucl] - counters[first][nucl]) != 0)
-            print "\n\n whoa NOT ZERO"
-            puts imp
-            results << imp
-            break
-          end
+    beginnings.each_with_index do |first, idx|
+      last = ends[idx]
+      impact.each do |nucl, imp|
+        if (counters[last + 1][nucl] - counters[first][nucl]) != 0
+          results << imp
+          break
         end
-      else
-        results << impact[sequence[first]]
       end
     end
+
     results
   end
 end
--- lesson-5/genomic-range-query/test.rb
+++ lesson-5/genomic-range-query/test.rb
@@ -14,11 +14,35 @@ describe GenomicRangeQuery do
     expect(solution(sequence, p, q)).must_equal expected_result
   end
 
-  it 'returns result for single nucleotide query' do
+  it 'returns result for a single-nucleotide query' do
     sequence = 'CAGCCTA'
     p = [2]
     q = [2]
     expected_result = [3]
+    expect(solution(sequence, p, q)).must_equal expected_result
+  end
+
+  it 'returns result for a two-nucleotide query' do
+    sequence = 'AACTGA'
+    p = [3]
+    q = [4]
+    expected_result = [3]
+    expect(solution(sequence, p, q)).must_equal expected_result
+  end
+
+  it 'returns result for several single-nucleotide queries' do
+    sequence = 'CAGCCTA'
+    p = [2, 3, 6]
+    q = [2, 3, 6]
+    expected_result = [3, 2, 1]
+    expect(solution(sequence, p, q)).must_equal expected_result
+  end
+
+  it 'returns result for all-the-same nucleotide sequence' do
+    sequence = 'CCCCCCC'
+    p = [0, 3, 5]
+    q = [1, 4, 6]
+    expected_result = [2, 2, 2]
     expect(solution(sequence, p, q)).must_equal expected_result
   end
 end
