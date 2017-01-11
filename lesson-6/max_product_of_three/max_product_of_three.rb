e1cb04fceb3c8483dff28daeab7f1d747aa33030
Bartek Rogalski <bartek.rogalski@gmail.com>
2017-01-11 17:23:58 +0100

    Implement solution

--- lesson-6/max_product_of_three/max_product_of_three.rb
+++ lesson-6/max_product_of_three/max_product_of_three.rb
@@ -1,31 +1,34 @@
 # https://codility.com/programmers/lessons/6-sorting/max_product_of_three/
+# This can be solved really, really quickly with sort!,
+# but for performance's sake:
 module MaxProductOfThree
   def solution(array)
-    max1 = -1_000
-    max2 = -1_000
-    max3 = -1_000
-    
+    max1, max2, max3 = -1_000
+    min1, min2 = 1_000
+
     array.each do |element|
-      print "element #{element}\n"
       if element > max1
-        print "bigger than max1!\n"
         max3 = max2
         max2 = max1
         max1 = element
-        next
-      end
-      if element > max2
-        print "bigger than max2!\n"
+      elsif element > max2
         max3 = max2
         max2 = element
-        next
-      end
-      if element > max3
-        print "bigger than max3!\n"
+      elsif element > max3
         max3 = element
+      end
+
+      if element < min1
+        min2 = min1
+        min1 = element
+      elsif element < min2
+        min2 = element
       end
     end
 
-    max1 * max2 * max3
+    max = max1 * max2 * max3
+    min = min1 * min2 * max1
+    return max if min < max
+    min
   end
 end
