257d7e62c0366df1d321088af875d5977546dc79
Bartek Rogalski <bartek.rogalski@gmail.com>
2017-02-02 11:18:12 +0100

    WIP first solution

---
+++ lesson-7/brackets/brackets.rb
@@ -0,0 +1,16 @@
+# https://codility.com/programmers/lessons/7-stacks_and_queues/brackets/
+module Brackets
+  def solution(strng)
+    ongoing = []
+    beginnings = { '{' => 0, '[' => 1, '(' => 2 }
+    endings    = { '}' => 0, ']' => 1, ')' => 2 }
+    strng.each_char.each do |char|
+      if beginnings[char].nil?
+        return 0 if beginnings[ongoing.pop] != endings[char]
+      else
+        ongoing << char
+      end
+    end
+    1
+  end
+end
